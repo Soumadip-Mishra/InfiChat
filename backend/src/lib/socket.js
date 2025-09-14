568943c5781a35412b9fe16652686d3a829d7a08
Soumadip Mishra <soumadipmishra2006@gmail.com>
2025-09-14 10:53:42 +0530

    Redis connection changed to URL for Render

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -2,7 +2,7 @@ import { Redis } from "ioredis";
 import { Server } from "socket.io";
 import { createAdapter } from "@socket.io/redis-adapter";
 
-import  http from "http";
+import http from "http";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -10,50 +10,49 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const CLIENT_URL = process.env.CLIENT_URL;
-const redis = new Redis({
-  host: "redis", 
-  port: 6379,
-});
 
-const pubClient = new Redis({
-  host: "redis", 
-  port: 6379,
-});
+console.log(process.env.REDIS_URL);
+
+const redis = new Redis(process.env.REDIS_URL);
+
+const pubClient = new Redis(process.env.REDIS_URL);
+
 const subClient = pubClient.duplicate();
-pubClient.on("error", err => console.error("REDIS ERROR", err));
-subClient.on("error", err => console.error("REDIS ERROR", err));
 
+pubClient.on("error", (err) => console.error("REDIS ERROR", err));
+subClient.on("error", (err) => console.error("REDIS ERROR", err));
 
-const io  = new Server(server,{
-    cors :{
-       origin: CLIENT_URL,
-    },
-    adapter: createAdapter(pubClient, subClient)
-})
+const io = new Server(server, {
+	cors: {
+		origin: CLIENT_URL,
+	},
+	adapter: createAdapter(pubClient, subClient),
+});
 
 export const getRecieverSocketId = async (userId) => {
-  return await redis.hget("online_users", userId);
+	return await redis.hget("online_users", userId);
 };
 
 io.on("connection", async (socket) => {
-  console.log("A user connected", socket.id);
-  const userId = socket.handshake.query.userId;
+	console.log("A user connected", socket.id);
+	const userId = socket.handshake.query.userId;
 
-  if (userId) {
-    await redis.hset("online_users", userId, socket.id);
-  }
+	if (userId) {
+		await redis.hset("online_users", userId, socket.id);
+	}
 
-  const allUsers = await redis.hkeys("online_users");
-  io.emit("onlineUsers", allUsers);
+	const allUsers = await redis.hkeys("online_users");
+	io.emit("onlineUsers", allUsers);
 
-  socket.on("disconnect", async () => {
-    console.log("A user disconnected", socket.id);
-    if (userId) {
-      await redis.hdel("online_users", userId);
-    }
+	socket.on("disconnect", async () => {
+		console.log("A user disconnected", socket.id);
+		if (userId) {
+			await redis.hdel("online_users", userId);
+		}
 
-    const allUsers = await redis.hkeys("online_users");
-    io.emit("onlineUsers", allUsers);
-  });
-});
-export {app,io,server}
+		const allUsers = await redis.hkeys("online_users");
+		io.emit("onlineUsers", allUsers);
+	}) ;
+}) ;
+
+export { app, io, server } ;
